303e82770c705cc1564c99da7b06ec4666ebd3ab
Ihamfp <12700765+Ihamfp@users.noreply.github.com>
2019-10-07 22:34:28 +0200

    Now working interactor. Define :interact() in your entities.

--- source/entity/interactor.lua
+++ source/entity/interactor.lua
@@ -1,15 +1,18 @@
 local entities = require("entities")
 local Solid = require("entity.solid")
+local interact = require("input").interact
 
 return Solid {
 	x = 0,
 	y = 0,
-	width=32,
-	height=32,
+	width=64,
+	height=64,
 	noCollision = true,
+	target = false,
 	
 	new = function(self,options)
 		Solid.new(self,options)
+		self:move(10000, 10000)
 	end,
 	
 	update = function(self,dt)
@@ -24,22 +27,38 @@ return Solid {
 		self.y = hyke.y
 		if hyke.direction == "up" then
 			self.y = self.y - hyke.height/2 - self.height - 4
+			self.x = self.x - (self.width-hyke.width)/2
 		elseif hyke.direction == "down" then
 			self.y = self.y + hyke.height/2 + 4
+			self.x = self.x - (self.width-hyke.width)/2
 		elseif hyke.direction == "left" then
 			self.x = self.x - hyke.width/2 - self.width - 4
+			self.y = self.y - (self.height-hyke.height)/2
 		elseif hyke.direction == "right" then
 			self.x = self.x + hyke.width/2 + 4
+			self.y = self.y - (self.height-hyke.height)/2
 		end
 		
-		self:move(self.x-oldx, self.y-oldy)
+		_, _, self.collisions = self.world:check(self, self.x, self.y)
+		self.world:update(self, self.x, self.y)
 		
+		local otherEntity = false
 		for _,c in ipairs(self.collisions) do
 			local o = c.other
 			
-			if o.__name ~= "hyke" then
+			if o.__name ~= "hyke" and o ~= self then
 				print(o.__name)
+				otherEntity = o
 			end
+		end
+		if otherEntity then
+			self.target = otherEntity
+		else
+			self.target = false
+		end
+		
+		if self.target and self.target.interact and input.interact:pressed() then
+			self.target:interact()
 		end
 	end,
 	
